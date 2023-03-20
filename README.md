63286ff013a1aeadf580bab527f6de64e03ea142
Nanna Ellegaard <nel@adm.ku.dk>
2023-03-20 10:32:53 +0100

    Setup

---
+++ README.md
@@ -0,0 +1,9 @@
+TYPO3 extension Tables
+==============================================================
+
+## Create accessible tables
+Content element to create tables with Bootstrap classes.
+
+## Developed for
+
+TYPO3 v. 11.5
---
+++ ext_emconf.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the package ku_tables.
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Tables',
+    'description' => 'Adds content element to create accessible tables.',
+    'category' => 'templates',
+    'constraints' => [
+        'depends' => [
+        ],
+        'conflicts' => [
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'UniversityOfCopenhagen\\KuTables\\' => 'Classes',
+        ],
+    ],
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'Nanna Ellegaard',
+    'author_email' => 'nel@adm.ku.dk',
+    'author_company' => 'University of Copenhagen',
+    'version' => '1.0.0',
+];
