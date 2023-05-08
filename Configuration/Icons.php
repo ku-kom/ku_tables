624124d0c46a1ed806085825a50e113dbfc21c03
Nanna Ellegaard <nel@adm.ku.dk>
2023-05-08 10:21:39 +0200

    Date adjustment

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -4,7 +4,7 @@
  * This file is part of the package ku_tables.
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
- * Sep 2022 Nanna Ellegaard, University of Copenhagen.
+ * May 2023 Nanna Ellegaard, University of Copenhagen.
  */
 
 /**
