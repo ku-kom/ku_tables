762c7cd4e989231c425c0af1b402d115879d54fe
Nanna Ellegaard <nel@adm.ku.dk>
2023-05-08 11:53:21 +0200

    Added comments

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -19,8 +19,8 @@ call_user_func(function ($extKey ='ku_tables', $contentType ='table') {
         'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:title'
     );
 
-    // Add checkbox element to enable datatables
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', [
+        // Add datatables checkbox element to enable datatables
         'tx_ku_tables_enable_datatable' => [
             'exclude' => true,
             'label' => 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_be.xlf:enable_datatable',
@@ -41,7 +41,7 @@ call_user_func(function ($extKey ='ku_tables', $contentType ='table') {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette(
         'tt_content',
         'tableconfiguration', // table palette name
-        'tx_ku_tables_enable_datatable',
+        'tx_ku_tables_enable_datatable', // New TCA
         'after:table_caption' // Existing TCA
     );
 });
